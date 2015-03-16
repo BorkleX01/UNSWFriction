d16d98f92f1a3da9375351d4c4bf67893ba207af
eugene <eugene.phang@gmail.com>
2015-03-17 10:58:44 +1100

    adjusted Model

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -2,23 +2,23 @@ define(['backbone'], function(Backbone) {
     var model = Backbone.Model.extend({
 
         defaults: {
-	    Options.AdjustSlope = 'false';
-	    Options.AdjustTruck = 'false';
-	    Options.Mode = 'none';
-	    Slope.FrictionCoefficient = 0.25;
-	    Slope.Angle = 30;
-	    Car.DistanceFromEdge = 5;
-	    Car.Mass = 1000;
-	    Truck.SideSupportLength = 5;
-	    Truck.DistanceFromEdge = 7.5;
-	    Truck.Mass = 3000;
-	    Data.CounterClockwiseMoment = 0;
-	    Data.ClockwiseMoment = 0;
-	    Data.LeftNormalForce = 0;
-	    Data.ForceCausingSliding = 0;
-	    Data.ForcePreventingSliding = 0;
-	    Data.MaxCarMass = 0;
-	    Data.MinCoefficientOfFriction = 0;
+	    AdjustSlope = 'false';
+	    AdjustTruck = 'false';
+	    Mode = 'none';
+	    FrictionCoefficient = 0.25;
+	    Angle = 30;
+	    DistanceFromEdge = 5;
+	    Mass = 1000;
+	    SideSupportLength = 5;
+	    DistanceFromEdge = 7.5;
+	    Mass = 3000;
+	    CounterClockwiseMoment = 0;
+	    ClockwiseMoment = 0;
+	    LeftNormalForce = 0;
+	    ForceCausingSliding = 0;
+	    ForcePreventingSliding = 0;
+	    MaxCarMass = 0;
+	    MinCoefficientOfFriction = 0;
 	    showSpeechBubble = 'false';
 	    showMomentArrowsOnSupportA = 'false';
 	    
